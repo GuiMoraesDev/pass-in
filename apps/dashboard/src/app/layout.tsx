f39dcd5750c86c673bc6c0db93df9ecacfb95452
Guilherme Moraes <guimoraes.dev@gmail.com>
2024-04-01 21:50:37 -0300

    feat: create page header

--- apps/dashboard/src/app/layout.tsx
+++ apps/dashboard/src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Roboto } from 'next/font/google';
 import '../styles/globals.css';
 import { twMerge } from 'tailwind-merge';
 
+import { Header } from '../components/molecules/Header';
+
 const roboto = Roboto({
   weight: ['100', '300', '400', '500', '700', '900'],
   subsets: ['latin'],
@@ -24,11 +26,12 @@ export default function RootLayout({
     <html lang="en" className="scroll-smooth">
       <body
         className={twMerge(
-          'flex h-[100dvh] w-full flex-col items-center gap-3 overflow-x-hidden bg-plum-900 text-white antialiased',
+          'flex h-[100dvh] w-full flex-col items-center gap-3 overflow-x-hidden bg-zinc-950 text-white antialiased',
           roboto.className
         )}
       >
         <div className="container mx-auto flex min-h-screen w-full flex-col gap-5 py-5 px-8">
+          <Header />
           {children}
         </div>
       </body>
--- apps/dashboard/src/app/participants/page.tsx
+++ apps/dashboard/src/app/participants/page.tsx
@@ -1,3 +1,20 @@
+import { Search } from 'lucide-react';
+
 export default function Participants() {
-  return <main className="flex h-full w-full flex-col">Hello, world!</main>;
+  return (
+    <main className="flex h-full w-full gap-4 flex-col">
+      <header className="flex w-full gap-3 items-center">
+        <h1 className="text-2xl font-bold">Participants</h1>
+
+        <span className="px-3 py-1.5 min-w-72 border border-white/10 rounded-lg text-sm flex items-center gap-3">
+          <Search className="size-4 text-emerald-300" />
+          <input
+            type="text"
+            placeholder="Search participants..."
+            className="flex-1 bg-transparent"
+          />
+        </span>
+      </header>
+    </main>
+  );
 }
