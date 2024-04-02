afe91e31893dd92924b8f3f6676f2bce47d35f1f
Guilherme Moraes <guimoraes.dev@gmail.com>
2024-04-01 22:13:52 -0300

    feat: add table component

--- apps/dashboard/src/app/participants/page.tsx
+++ apps/dashboard/src/app/participants/page.tsx
@@ -1,6 +1,17 @@
-import { Search } from 'lucide-react';
+import { Search, MoreHorizontal } from 'lucide-react';
+
+import { Checkbox } from '../../components/atoms/Checkbox';
+import { Table } from '../../components/atoms/Table';
 
 export default function Participants() {
+  const participants = Array.from({ length: 6 }).map((_, index) => ({
+    code: '123456',
+    name: 'John Doe',
+    email: 'example@email.com',
+    registrationDate: '2021-01-01',
+    checkInDate: '2021-01-01',
+  }));
+
   return (
     <main className="flex h-full w-full gap-4 flex-col">
       <header className="flex w-full gap-3 items-center">
@@ -15,6 +26,47 @@ export default function Participants() {
           />
         </span>
       </header>
+
+      <Table.Root>
+        <Table.Header>
+          <Table.Row>
+            <Table.Head>
+              <Checkbox />
+            </Table.Head>
+            <Table.Head>Code</Table.Head>
+            <Table.Head>Participant</Table.Head>
+            <Table.Head>Registration date</Table.Head>
+            <Table.Head>Check-in date</Table.Head>
+            <Table.Head />
+          </Table.Row>
+        </Table.Header>
+
+        <Table.Body>
+          {participants.map((participant, index) => (
+            <Table.Row key={participant.email + index}>
+              <Table.Cell>
+                <Checkbox />
+              </Table.Cell>
+
+              <Table.Cell>{participant.code}</Table.Cell>
+
+              <Table.Cell>
+                <span className="flex flex-col">
+                  {participant.name}
+                  <small>{participant.email}</small>
+                </span>
+              </Table.Cell>
+              <Table.Cell>{participant.registrationDate}</Table.Cell>
+              <Table.Cell>{participant.checkInDate}</Table.Cell>
+              <Table.Cell>
+                <button className="bg-black/20 border border-white/10 rounded-md p-1.5">
+                  <MoreHorizontal className="size-4" />
+                </button>
+              </Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table.Root>
     </main>
   );
 }
